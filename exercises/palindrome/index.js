bd0b5fbbd254d3aabc033e810a3be3c4888c93b0
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2018-11-27 10:34:17 +0100

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,12 +8,18 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const reversed = str
-    .split('')
-    .reverse()
-    .join('');
-
-  return str === reversed;
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//
+//   return str === reversed;
+// }
