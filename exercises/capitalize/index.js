9d2e31b9c9b9d27bbbbe759c38d5fcccde04cea2
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2018-11-29 12:30:16 +0100

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let capitalized = [];
+
+  str.split(' ').forEach((word, i) => {
+    capitalized.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+
+  return capitalized.join(' ');
+}
 
 module.exports = capitalize;
