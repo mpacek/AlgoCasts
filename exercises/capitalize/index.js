4a48e7d34f823542b946efd2384d5f63c07b3515
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2018-11-29 12:36:06 +0100

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,13 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  let capitalized = [];
+  let words = [];
 
-  str.split(' ').forEach((word, i) => {
-    capitalized.push(word.charAt(0).toUpperCase() + word.slice(1));
-  });
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
 
-  return capitalized.join(' ');
+  return words.join(' ');
 }
 
 module.exports = capitalize;
